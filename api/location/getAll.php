a65a9cb4623b54666ae23e7a5fe7dd376571a7f3
Ezen <82026690+ezengaston@users.noreply.github.com>
2022-05-12 19:09:12 +0100

    getAll method added

---
+++ api/location/getAll.php
@@ -0,0 +1,34 @@
+<?php
+
+header('Access-Control-Allow-Origin: *');
+header('Content-Type: application/json');
+
+include_once '../../config/Database.php';
+include_once '../../models/Locations.php';
+
+$database = new Database();
+$db = $database->connect();
+
+$locations = new Locations($db);
+
+$results = $locations->getAll();
+
+$num = $results->rowCount();
+
+if ($num > 0) {
+    $locations_arr = array('data' => array());
+    $row = $results->fetch(PDO::FETCH_ASSOC);
+
+    while ($row = $results->fetch(PDO::FETCH_ASSOC)) {
+        $location = array(
+            'id' => $row['id'],
+            'location_name' => $row['location_name']
+        );
+
+        array_push($locations_arr['data'], $location);
+    }
+
+    echo json_encode($locations_arr);
+} else {
+    echo json_encode(array('message' => 'No results found'));
+}
--- models/Locations.php
+++ models/Locations.php
@@ -1,1 +1,26 @@
 <?php
+
+class Locations
+{
+    private $conn;
+    private $table = 'locations';
+
+    public $id;
+    public $location_name;
+
+    public function __construct($db)
+    {
+        $this->conn = $db;
+    }
+
+    public function getAll()
+    {
+        $query = "SELECT * FROM $this->table";
+
+        $stmt = $this->conn->prepare($query);
+
+        $stmt->execute();
+
+        return $stmt;
+    }
+}
